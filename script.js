292b1f05c490476d6ec2151fbcba50cd5c55854f
Danny Simms <djfsimms@hotmail.com>
2023-01-04 20:19:55 -0330

    Add html boilerplate with link to script.js
    This commit updates index.html to give it the basic functional html
    boilerplate. It also updates the script.js with pseudo code to help
    guide through the necessary code creation.

---
+++ script.js
@@ -0,0 +1,11 @@
+// Ask the user to chose rock, paper or scissors
+// Calculate the computer's random choice 
+// Compare the user's and computer's selections
+// Set the round to 1, increment round, end game after 5 rounds
+// If the user's choice is the same as computer's choice output "It's a tie!"
+// If user's choice is rock and computer's choice is scissors output "You win!"
+// If user's choice is rock and computer's choice is paper output "You lose"
+// If user's choice is paper and computer's choice is rock output "You win!"
+// If user's choice is paper and computer's choice is scissors output "You lose"
+// If user's choice is scissors and computer's choice is paper output "You win!"
+// If user's choice is scissors and computer's choice is rock output "You lose"
