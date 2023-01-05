90c52709ef15c962295aafc95dbb0cafe7d37d50
Danny Simms <djfsimms@hotmail.com>
2023-01-04 21:02:16 -0330

    Add user's and computer's selection
    Adds JS to allow the user to chose rock, paper or scissors, and
    generates a random selection for the computer.

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // Ask the user to chose rock, paper or scissors
-// Calculate the computer's random choice 
-// Compare the user's and computer's selections
+const playerSelection = prompt("Chose rock, paper, or scissors: ");
+console.log(playerSelection);
+
+// Calculate the computer's random choice
+const computerChoice = getComputerChoice();
+console.log(computerChoice);
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let randomChoice;
+
+    if (randomNumber === 1) {
+        randomChoice = "rock";
+    } else if (randomNumber === 2) {
+        randomChoice = "paper";
+    } else {
+        randomChoice = "scissors";
+    }
+
+    return randomChoice;
+}
+
 // Set the round to 1, increment round, end game after 5 rounds
+// Compare the user's and computer's selections
 // If the user's choice is the same as computer's choice output "It's a tie!"
 // If user's choice is rock and computer's choice is scissors output "You win!"
 // If user's choice is rock and computer's choice is paper output "You lose"
