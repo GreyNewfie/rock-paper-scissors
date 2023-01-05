d21d4919385b2f7d78846ae0ac2be4910ce6b097
Danny Simms <djfsimms@hotmail.com>
2023-01-04 21:47:21 -0330

    Add funtion to play a single round of the game
    Add conditional to determine the winner of a single round of rock,
    paper, scissors.

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const playerSelection = prompt("Chose rock, paper, or scissors: ");
 console.log(playerSelection);
 
 // Calculate the computer's random choice
-const computerChoice = getComputerChoice();
-console.log(computerChoice);
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
 
+// Generates a random number from 1 to 3 and assigns rock, paper or scissors based on the number
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     let randomChoice;
@@ -22,11 +23,39 @@ function getComputerChoice() {
 }
 
 // Set the round to 1, increment round, end game after 5 rounds
+
 // Compare the user's and computer's selections
-// If the user's choice is the same as computer's choice output "It's a tie!"
-// If user's choice is rock and computer's choice is scissors output "You win!"
-// If user's choice is rock and computer's choice is paper output "You lose"
-// If user's choice is paper and computer's choice is rock output "You win!"
-// If user's choice is paper and computer's choice is scissors output "You lose"
-// If user's choice is scissors and computer's choice is paper output "You win!"
-// If user's choice is scissors and computer's choice is rock output "You lose"
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+
+    // If the user's choice is the same as computer's choice output "It's a tie!"
+    if (playerChoice === computerSelection) {
+        alert("It's a tie");
+
+    // If user's choice is rock and computer's choice is scissors output "You win!"
+    } else if (playerChoice === "rock" && computerSelection === "scissors") {
+        alert("You win!");
+
+    // If user's choice is rock and computer's choice is paper output "You lose"
+    } else if (playerChoice === "rock" && computerSelection === "paper") {
+        alert("You lose");
+    
+    // If user's choice is paper and computer's choice is rock output "You win!"
+    } else if (playerChoice === "paper" && computerSelection === "rock") {
+        alert("You win!");
+
+    // If user's choice is paper and computer's choice is scissors output "You lose"
+    } else if (playerChoice === "paper" && computerSelection === "scissors") {
+        alert("You lose");
+
+    // If user's choice is scissors and computer's choice is paper output "You win!"    
+    } else if (playerChoice === "scissors" && computerSelection === "paper") {
+        alert("You win!");
+
+    // If user's choice is scissors and computer's choice is rock output "You lose"
+    } else if (playerChoice === "scissors" && computerSelection === "rock") {
+        alert("You lose");
+    } else {
+        alert("You entered an invalid response");
+    }
+}
