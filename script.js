884c22f3d4608e5afd04df3c598b9687d9ae21ec
Danny Simms <djfsimms@hotmail.com>
2023-02-17 17:50:49 -0330

    Add buttons for player and UI Improvements
    Adds rock, paper and scissor buttons for player to choose from. Also,
    adds UI functionality to display the current score and alert to
    announce the winner.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Ask the user to chose rock, paper or scissors
-const playerSelection = prompt("Chose rock, paper, or scissors: ");
-console.log(playerSelection);
+let playerWins = 0;
+let computerWins = 0;
 
-// Calculate the computer's random choice
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-
-//Starts a game of 5 rounds
-playGame(playerSelection, computerSelection);
+document.addEventListener("click", (e) => {
+    let playerSelection = e.target.innerText;
+    console.log(playerSelection);
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    playRound(playerSelection, computerSelection);
+});
 
 // Generates a random number from 1 to 3 and assigns rock, paper or scissors based on the number
 function getComputerChoice() {
@@ -21,56 +21,54 @@ function getComputerChoice() {
     } else {
         randomChoice = "scissors";
     }
-
     return randomChoice;
-}
-
-// Play a game of 5 rounds, and anounce the winner of the game after 5 rounds.
-function playGame(playerSelection, computerSelection) {  
-    
-    let playerWins = 0;
-    let computerWins = 0;
-
-    for (i = 1; i <= 5; i++) { 
-       let roundWinner = playRound(playerSelection, computerSelection);
-       if (roundWinner === "player") {
-            playerWins++;
-       } else if (roundWinner === "computer") {
-            computerWins++;
-       }
-
-       playerSelection = prompt("Chose rock, paper, or scissors: ");
-       computerSelection = getComputerChoice();
-    }
-
-    return playerWins > computerWins ? alert("You won the game!")
-         : computerWins > playerWins ? alert("You lost the game")
-         : alert("It's a tie game")
 }
 
 // Compare the user's and computer's selections
 function playRound(playerSelection, computerSelection) {
-    let playerChoice = playerSelection.toLowerCase();
-    let wonRound;
+    playerSelection = playerSelection.toLowerCase();
+    let displayResult = document.getElementById("result");
+    console.log(displayResult);
+    let playerScore = document.getElementById("playerScore");
+    let computerScore = document.getElementById("computerScore");
 
-    if (playerChoice === computerSelection) {
-        alert("This round is a tie");
+    if (playerWins < 5 && computerWins < 5) {
+        if (playerSelection === computerSelection) {
+            displayResult.innerText = "This round is a tie";
+            playerScore.innerText = `Player ${playerWins}`;
+            computerScore.innerText = `Computer ${computerWins}`;
 
-    } else if (playerChoice === "rock" && computerSelection === "scissors" 
-        || playerChoice === "paper" && computerSelection === "rock" 
-        || playerChoice === "scissors" && computerSelection === "paper") {
-        alert("You win this round!");
-        winner = "player";
 
-    } else if (playerChoice === "rock" && computerSelection === "paper"
-        || playerChoice === "paper" && computerSelection === "scissors"
-        || playerChoice === "scissors" && computerSelection === "rock") {
-        alert("You lose this round");
-        winner = "computer";
-
-    } else {
-        alert("You entered an invalid response");
-    }
-
-    return winner;
+        } else if (playerSelection === "rock" && computerSelection === "scissors" 
+            || playerSelection === "paper" && computerSelection === "rock" 
+            || playerSelection === "scissors" && computerSelection === "paper") {
+            displayResult.innerText = "You win this round!";
+            playerWins++;
+            playerScore.innerText = `Player ${playerWins}`;
+            computerScore.innerText = `Computer ${computerWins}`;
+    
+        } else if (playerSelection === "rock" && computerSelection === "paper"
+            || playerSelection === "paper" && computerSelection === "scissors"
+            || playerSelection === "scissors" && computerSelection === "rock") {
+            displayResult.innerText = "You lose this round";
+            computerWins++;
+            playerScore.innerText = `Player ${playerWins}`;
+            computerScore.innerText = `Computer ${computerWins}`;
+    
+        } else {
+            alert("You entered an invalid response");
+        }
+    } 
+    
+        if (playerWins === 5) {
+            playerScore.innerText = `Player ${playerWins}`;
+            alert("You win!");
+            playerWins = 0;
+            computerWins = 0;
+        } else if (computerWins === 5) {
+            computerScore.innerText = `Computer ${computerWins}`;
+            alert("You lose");
+            playerWins = 0;
+            computerWins = 0;
+        }
 }
