3bf495ffaa9e75806bf5a0767c2c334961392a70
Danny Simms <djfsimms@hotmail.com>
2023-02-18 11:37:07 -0330

    Update UI and website style
    Adds images to buttons and website style including background colors,
    fonts, and layout.

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let playerWins = 0;
 let computerWins = 0;
 
+// const buttons = document.querySelectorAll(".btn");
+// console.log(document.querySelector(".btn"));
+
+// buttons.forEach((button) => button.addEventListener("click", startGame));
+
+// function startGame(e) {
+//     let playerSelection = e.target.name;
+//     let computerSelection = getComputerChoice();
+//     playRound(playerSelection, computerSelection);
+// }
+
 document.addEventListener("click", (e) => {
-    let playerSelection = e.target.innerText;
-    console.log(playerSelection);
+    console.log(e.target.name);
+    let playerSelection = e.target.name;
     let computerSelection = getComputerChoice();
-    console.log(computerSelection);
+    // console.log(computerSelection);
     playRound(playerSelection, computerSelection);
 });
 
@@ -28,15 +39,14 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     let displayResult = document.getElementById("result");
-    console.log(displayResult);
     let playerScore = document.getElementById("playerScore");
     let computerScore = document.getElementById("computerScore");
 
     if (playerWins < 5 && computerWins < 5) {
         if (playerSelection === computerSelection) {
             displayResult.innerText = "This round is a tie";
-            playerScore.innerText = `Player ${playerWins}`;
-            computerScore.innerText = `Computer ${computerWins}`;
+            playerScore.innerText = `${playerWins}`;
+            computerScore.innerText = `${computerWins}`;
 
 
         } else if (playerSelection === "rock" && computerSelection === "scissors" 
@@ -44,16 +54,16 @@ function playRound(playerSelection, computerSelection) {
             || playerSelection === "scissors" && computerSelection === "paper") {
             displayResult.innerText = "You win this round!";
             playerWins++;
-            playerScore.innerText = `Player ${playerWins}`;
-            computerScore.innerText = `Computer ${computerWins}`;
+            playerScore.innerText = `${playerWins}`;
+            computerScore.innerText = `${computerWins}`;
     
         } else if (playerSelection === "rock" && computerSelection === "paper"
             || playerSelection === "paper" && computerSelection === "scissors"
             || playerSelection === "scissors" && computerSelection === "rock") {
             displayResult.innerText = "You lose this round";
             computerWins++;
-            playerScore.innerText = `Player ${playerWins}`;
-            computerScore.innerText = `Computer ${computerWins}`;
+            playerScore.innerText = `${playerWins}`;
+            computerScore.innerText = `${computerWins}`;
     
         } else {
             alert("You entered an invalid response");
@@ -61,12 +71,12 @@ function playRound(playerSelection, computerSelection) {
     } 
     
         if (playerWins === 5) {
-            playerScore.innerText = `Player ${playerWins}`;
+            playerScore.innerText = `${playerWins}`;
             alert("You win!");
             playerWins = 0;
             computerWins = 0;
         } else if (computerWins === 5) {
-            computerScore.innerText = `Computer ${computerWins}`;
+            computerScore.innerText = `${computerWins}`;
             alert("You lose");
             playerWins = 0;
             computerWins = 0;
